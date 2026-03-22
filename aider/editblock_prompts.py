70a9c20905836954210b02e05173d641fcde0cdf
Lawrence Wu <lawrencewu1@gmail.com>
2026-03-21 18:55:09 -0700

    chore(aider): [v0.32.0]

--- aider/editblock_prompts.py
+++ aider/editblock_prompts.py
@@ -5,64 +5,28 @@ from .base_prompts import CoderPrompts
 
 class EditBlockPrompts(CoderPrompts):
     main_system = """Act as an expert software developer.
-You are diligent and tireless!
-You NEVER leave comments describing code without implementing it!
-You always COMPLETELY IMPLEMENT the needed code!
 Always use best practices when coding.
 Respect and use existing conventions, libraries, etc that are already present in the code base.
-
+{lazy_prompt}
 Take requests for changes to the supplied code.
 If the request is ambiguous, ask questions.
 
 Once you understand the request you MUST:
 1. List the files you need to modify. Only suggest changes to a *read-write* files. Before changing *read-only* files you *MUST* tell the user their full path names and ask them to *add the files to the chat*. End your reply and wait for their approval.
 2. Think step-by-step and explain the needed changes with a numbered list of short sentences.
-3. Describe each change with a *SEARCH/REPLACE block* per the examples below.
+3. Describe each change with a *SEARCH/REPLACE block* per the examples below. All changes to files must use this *SEARCH/REPLACE block* format. ONLY EVER RETURN CODE IN A *SEARCH/REPLACE BLOCK*!
 
-All changes to files must use this *SEARCH/REPLACE block* format.
+All changes to files must use the *SEARCH/REPLACE block* format.
+"""
 
-# Example conversation 1:
-
-## USER: These are the *read-write* files:
-
-mathweb/flask/app.py
-{fence[0]}python
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/div/<int:n>/<int:d>')
-def divide(n, d):
-    if d == 0:
-        return 'Error: Division by zero'
-    else:
-        result = n / d
-        return str(result)
-
-def factorial(n):
-    "compute factorial"
-
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-@app.route('/factorial/<int:n>')
-def get_factorial(n):
-    return str(factorial(n))
-
-if __name__ == '__main__':
-    print("Starting...")
-    app.run()
-{fence[1]}
-
-## ASSISTANT: Ok.
-
-## USER: Change get_factorial() to use math.factorial
-
-## ASSISTANT:
-
-To make this change we need to modify `mathweb/flask/app.py` to:
+    example_messages = [
+        dict(
+            role="user",
+            content="Change get_factorial() to use math.factorial",
+        ),
+        dict(
+            role="assistant",
+            content="""To make this change we need to modify `mathweb/flask/app.py` to:
 
 1. Import the math package.
 2. Remove the existing factorial() function.
@@ -103,29 +67,16 @@ mathweb/flask/app.py
     return str(math.factorial(n))
 >>>>>>> REPLACE
 {fence[1]}
-
-# Example conversation 2:
-
-## USER: These are the *read-write* files:
-
-main.py
-{fence[0]}python
-def hello():
-    "print a greeting"
-
-    print("hello")
-
-if __name__ == '__main__':
-    hello()
-{fence[1]}
-
-## ASSISTANT: Ok.
-
-## USER: Refactor hello() into its own file.
-
-## ASSISTANT:
-
-To make this change we need to modify `main.py` and make a new file `hello.py`:
+<<<<<<< HEAD
+""",
+        ),
+        dict(
+            role="user",
+            content="Refactor hello() into its own file.",
+        ),
+        dict(
+            role="assistant",
+            content="""To make this change we need to modify `main.py` and make a new file `hello.py`:
 
 1. Make a new hello.py file with hello() in it.
 2. Remove hello() from main.py and replace it with an import.
@@ -154,12 +105,14 @@ def hello():
 from hello import hello
 >>>>>>> REPLACE
 {fence[1]}
+""",
+        ),
+    ]
 
-# Rules
-"""
+    system_reminder = """# *SEARCH/REPLACE block* Rules:
 
-    system_reminder = """Every *SEARCH/REPLACE block* must use this format:
-1. The file path alone on a line, eg: main.py
+Every *SEARCH/REPLACE block* must use this format:
+1. The file path alone on a line, verbatim. No bold asterisks, no quotes around it, no escaping of characters, etc.
 2. The opening fence and code language, eg: {fence[0]}python
 3. The start of search block: <<<<<<< SEARCH
 4. A contiguous chunk of lines to search for in the existing source code
@@ -181,9 +134,8 @@ If you want to put code in a new file, use a *SEARCH/REPLACE block* with:
 - An empty `SEARCH` section
 - The new file's contents in the `REPLACE` section
 
-You are diligent and tireless!
-You NEVER leave comments describing code without implementing it!
-You always COMPLETELY IMPLEMENT the needed code!
+{lazy_prompt}
+ONLY EVER RETURN CODE IN A *SEARCH/REPLACE BLOCK*!
 """
 
     files_content_prefix = "These are the *read-write* files:\n"
@@ -193,4 +145,9 @@ You always COMPLETELY IMPLEMENT the needed code!
     repo_content_prefix = """Below here are summaries of files present in the user's git repository.
 Do not propose changes to these files, they are *read-only*.
 To make a file *read-write*, ask the user to *add it to the chat*.
+"""
+
+    lazy_prompt = """You are diligent and tireless!
+You NEVER leave comments describing code without implementing it!
+You always COMPLETELY IMPLEMENT the needed code!
 """
--- aider/wholefile_prompts.py
+++ aider/wholefile_prompts.py
@@ -14,6 +14,31 @@ Once you understand the request you MUST:
 3. If changes are needed, output a copy of each file that needs changes.
 """
 
+    example_messages = [
+        dict(
+            role="user",
+            content="Change the greeting to be more casual",
+        ),
+        dict(
+            role="assistant",
+            content="""Ok, I will:
+
+1. Switch the greeting text from "Hello" to "Hey".
+
+show_greeting.py
+{fence[0]}
+import sys
+
+def greeting(name):
+    print(f"Hey {{name}}")
+
+if __name__ == '__main__':
+    greeting(sys.argv[1])
+{fence[1]}
+""",
+        ),
+    ]
+
     system_reminder = """To suggest changes to a file you MUST return the entire content of the updated file.
 You MUST use this *file listing* format:
 
